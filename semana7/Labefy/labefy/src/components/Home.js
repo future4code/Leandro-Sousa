bcff85e71a1e90f1b3823298cb5adceb9b3917a4
megall127 <leandro.wilker128@gmail.com>
2021-07-30 19:05:38 -0300

    Lista de de Playlists Renderizada

--- semana7/Labefy/labefy/src/components/Home.js
+++ semana7/Labefy/labefy/src/components/Home.js
@@ -25,11 +25,13 @@ height: 30px;
 
 export default class Home extends React.Component {
     state = {
-        playLists :[
-            {
-            name:"Forró"
-        },
-    ],
+        playlists : [],
+        inputPlayList:"",
+        playlistId:"",
+        tracks:[],
+    
+
+    
     inputNome: "",
     inputArtist:"",
     inputUrl:""
@@ -44,13 +46,60 @@ export default class Home extends React.Component {
         axios
         .get(url, headers)
         .then((res) => {
-            this.setState({playLists: res.data.result})
+            this.setState({playlists: res.data.result.list})
+            console.log(res.data)
+        })
+        .catch((error) => {
+            alert(error)
+        })
+    }
+
+    novaPlaylist = () => {
+        const body = {
+            name: this.state.inputPlayList
+        }
+        axios
+        .post(url, body, headers)
+        .then((res) => {
+            alert("Playlist Criada")
+            this.setState({inputPlayList:""})
             console.log(res)
         })
         .catch((error) => {
             alert(error)
         })
     }
+    
+    pegarMusica = () => {
+        axios
+        .get(url, headers)
+        .then((res) =>{
+            this.setState({tracks: res})
+        })
+        .catch((error) => {
+            alert(error)
+        })
+    }
+
+    novaMusica = () => {
+        const body = {
+            name: this.state.inputNome,
+            artist :this.state.inputArtist,
+            url: this.state.inputPlayList
+        }
+        axios
+        .post(url, this.state.playlistId, body, headers)
+        .then((res) => {
+            alert("Música adicionada com sucesso!")
+            this.setState({inputNome:""})
+            this.setState({inputArtist:""})
+            this.setState({inputUrl:""})
+        })
+        .catch((error) => {
+            alert(error)
+        })
+    }
+
 
     mudaInputNome = (e) => {
         this.setState({inputNome: e.target.value})
@@ -64,39 +113,38 @@ export default class Home extends React.Component {
         this.setState({inputUrl: e.target.value})
     }
 
-    novaMusica = () => {
-        const body = {
-            name:"string",
-            artist:"string",
-            url:"string"
-        }
-        axios
-        .post(url, body ,headers)
-        .then((res) => {
-            alert("Música adicionada com sucesso!")
-            this.setState({inputNome:""})
-            this.setState({inputArtist:""})
-            this.setState({inputUrl:""})
-        })
-        .catch((error) => {
-            alert(error.data.message)
-        })
+    mudaInputPlayList = (e) => {
+        this.setState({inputPlayList: e.target.value})
     }
 
-    render(){
-        // const componentePlayList = this.state.playLists.map((n) => {
-        //     return <li key={n.id}>{n.name}</li>;
-        // })
+    mudaInputPlayListId = (e) => {
+        this.setState({playlistId: e.target.value})
+    }
 
+
+
+    render(){
+         const playlists = this.state.playlists.map(playlist => {
+             return <p key={playlist.id}>{playlist.name} - {playlist.id}</p>
+                   
+         })
         return(
             <div>
                 <div>
             <h2>Suas Playlists</h2>
+            {playlists}
+            <h3>Criar Playlist</h3>
+            <input placeholder="Nome da PlayList" 
+                value={this.state.inputPlayList}
+                onChange={this.mudaInputPlayList}
+            />
+            <button onClick={this.novaPlaylist}>Criar PlayList</button>
+            
            
 
                 </div>
                 <Inputs>
-            <h3>Criar Playlists</h3>
+            <h3>Adicionar Músicas</h3>
             <input placeholder="Nome da Música" 
                 value={this.state.inputNome}
                 onChange={this.mudaInputNome}
@@ -111,7 +159,12 @@ export default class Home extends React.Component {
                 onChange={this.mudaInputUrl}
                 
             />
-            <button onClick={this.novaMusica}>Enviar Música</button>
+            <input placeholder="Id da Playlist"
+                value={this.state.playlistId}
+                onChange={this.mudaInputPlayListId}
+                
+            />
+            <button onClick={this.novaMusica(this.props.playlistId)}>Enviar Música</button>
             </Inputs>
 
             </div>
